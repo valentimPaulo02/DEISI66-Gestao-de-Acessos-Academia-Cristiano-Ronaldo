4863b8eab24e5896ea03b9a6e73a82d96a9cb978
Valentim <valentimspaulo@gmail.com>
2024-01-11 11:03:03 +0000

    new sql dump

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 CORS(app)
 
 app.config['MYSQL_USER'] = "root"
-app.config['MYSQL_PASSWORD'] = "frizze2002"
+app.config['MYSQL_PASSWORD'] = "sportingtfc2023"
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_DB'] = "academiasporting"
 app.config['MYSQL_CURSORCLASS'] = "DictCursor"
