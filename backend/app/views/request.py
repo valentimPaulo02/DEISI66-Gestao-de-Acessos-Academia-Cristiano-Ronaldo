1d9b912cdefeb4c3c581a67e3625bd8cbd50c6d4
valentimPaulo02 <valentimspaulo@gmail.com>
2024-01-15 09:24:30 +0000

    fixed database and requests ready

--- backend/app/views/request.py
+++ backend/app/views/request.py
@@ -40,3 +40,37 @@ def makeTemporaryRequest():
         mysql.connection.commit()
         
         return {"success":True}
+    
+
+@request_bp.route('/makeWeekendRequest', methods=["POST"])
+def makeWeekendRequest():
+    if request.method=="POST":
+
+        ptr = mysql.connection.cursor()
+        data = request.get_json()
+
+        token = str(data["token"])
+        leave_date = str(data["leave_date"])
+        leave_time = str(data["leave_time"])
+        supervisor = str(data["supervisor"])
+        transport = str(data["transport"])
+        destiny = str(data["destiny"])
+        state = "pending"
+        validated = False
+        date = datetime.today().date()
+
+        query = "SELECT * FROM user WHERE token=%s;"
+        values = (token,)
+        ptr.execute(query, values)
+        info = ptr.fetchall()
+
+        user_id = info[0]["user_id"]
+        leave_date = datetime.fromisoformat(leave_date).date()
+        leave_time = datetime.strptime(leave_time, "%H:%M").time()
+
+        query = "INSERT INTO weekendrequest (user_id, state, validated, date, leave_date, leave_time, supervisor, transport_out, destiny) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);"
+        values = (user_id, state, validated, date, leave_date, leave_time, supervisor, transport, destiny)
+        ptr.execute(query, values)
+        mysql.connection.commit()
+        
+        return {"success":True}
