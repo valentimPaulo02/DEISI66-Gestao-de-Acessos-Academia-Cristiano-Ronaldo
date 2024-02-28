6952e97e9f7d79e822242a5be688e5020f82dd71
joao-anacleto-a22202260 <joaopranacleto@gmail.com>
2024-02-28 14:59:35 +0000

    faz consultar_pedido.dart

--- frontend/lib/pages/consultar_pedido.dart
+++ frontend/lib/pages/consultar_pedido.dart
@@ -1,17 +1,18 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
 import '../componentes/app_pages.dart';
 import '../componentes/custom_app_bar.dart';
 import '../main.dart';
 
+// Definição da classe Pedido
 class Pedido {
   int requestId;
   int userId;
   String username;
   String state;
   String date;
-  String tipo;
 
   Pedido({
     required this.requestId,
@@ -19,10 +20,10 @@ class Pedido {
     required this.username,
     required this.state,
     required this.date,
-    required this.tipo,
   });
 }
 
+// Definição da classe ConsultarPedidoPage
 class ConsultarPedidoPage extends StatefulWidget {
   const ConsultarPedidoPage({Key? key}) : super(key: key);
 
@@ -40,6 +41,7 @@ class _ConsultarPedidoPageState extends State<ConsultarPedidoPage> {
   void initState() {
     super.initState();
     _getPedidosFromBackend();
+   // _testPedidos();
   }
 
   Future<void> _getPedidosFromBackend() async {
@@ -59,16 +61,6 @@ class _ConsultarPedidoPageState extends State<ConsultarPedidoPage> {
       }
     }
 
-/*
-    pedidosAll.clear();
-    pedidosAll.add(Pedido(
-        requestId: 20,
-        userId: 100,
-        username: "joao",
-        state: "RECUSADO",
-        date: "2002-11-10",tipo: "Temporary"));
-        */
-
     final response = await http.get(Uri.parse(url), headers: {
       "Content-Type": "application/json",
       'token': getToken(),
@@ -86,7 +78,7 @@ class _ConsultarPedidoPageState extends State<ConsultarPedidoPage> {
                       userId: pedido['user_id'],
                       username: pedido['username'],
                       state: pedido['state'],
-                      date: pedido['date'], tipo: '',
+                      date: pedido['date'],
                     ))
                 .toList();
           } else {
@@ -96,7 +88,7 @@ class _ConsultarPedidoPageState extends State<ConsultarPedidoPage> {
                       userId: pedido['user_id'],
                       username: pedido['username'],
                       state: pedido['state'],
-                      date: pedido['date'], tipo: '',
+                      date: pedido['date'],
                     ))
                 .toList();
           }
@@ -108,6 +100,40 @@ class _ConsultarPedidoPageState extends State<ConsultarPedidoPage> {
       print('Erro de Net ao procurar lista de pedidos');
     }
   }
+/*
+  Future<void> _testPedidos() async {
+    setState(() {
+      pedidosAll.clear();
+
+      if (tipoPedido == "Temporary") {
+        pedidosAll.add(Pedido(
+          requestId: 1,
+          userId: 101,
+          username: "ESGAIO",
+          state: "REPROVADO",
+          date: "2022-02-28",
+        ));
+
+        pedidosAll.add(Pedido(
+          requestId: 2,
+          userId: 102,
+          username: "ADAN",
+          state: "REPROVADO",
+          date: "2022-03-01",
+        ));
+      }else{
+        pedidosAll.add(Pedido(
+          requestId: 3,
+          userId: 103,
+          username: "VIKTOR GYOKERES",
+          state: "APROVADO",
+          date: "2022-03-05",
+        ));
+      }
+    });
+  }
+
+ */
 
   void _navigateToPage(int index) {
     setState(() {
