0b5cfe4b29dae03fc21225d51f2b34b6335a5a61
joao-anacleto-a22202260 <joaopranacleto@gmail.com>
2024-03-17 11:21:54 +0000

    atualiza lista de Supervisores

--- frontend/lib/pages/lista_supervisores.dart
+++ frontend/lib/pages/lista_supervisores.dart
@@ -1,32 +1,26 @@
 import 'dart:convert';
-import 'package:deisi66/componentes/textfield.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import '../componentes/app_bar_with_back.dart';
 import '../main.dart';
 import '../componentes/app_pages.dart';
 import '../componentes/custom_app_bar.dart';
 import '../componentes/navigation_manager.dart';
-import 'package:http/http.dart' as http;
+import 'package:deisi66/componentes/textfield.dart' as customTextField;
 
 class Supervisor {
+  int id;
   String name;
   String surname;
 
   Supervisor({
+    required this.id,
     required this.name,
     required this.surname,
   });
-
-  void update({
-    String? name,
-    String? surname,
-    String? category,
-  }) {
-    this.name = name ?? this.name;
-    this.surname = surname ?? this.surname;
-  }
 }
 
+//Página Lista de Supervisores
 class ListaSupervisoresPage extends StatefulWidget {
   const ListaSupervisoresPage({Key? key}) : super(key: key);
 
@@ -43,24 +37,20 @@ class _ListaSupervisoresPageState extends State<ListaSupervisoresPage> {
   void initState() {
     super.initState();
 
-    if(getRole()=='supervisor'){
-      currentPage = 3;
-    }
     navigationManager = NavigationManager(context, currentPage: currentPage);
 
-
     supervisores = [
-      Supervisor(name: 'João', surname: 'Anacleto'),
-      Supervisor(name: 'Valentim', surname: 'Paulo')
+      Supervisor(id: 1, name: 'João', surname: 'Anacleto'),
+      Supervisor(id: 2, name: 'Valentim', surname: 'Paulo'),
+      Supervisor(id: 3, name: 'test', surname: 'aaa')
     ];
-
-
 
     _getSupervisorList();
   }
 
   Future<void> _getSupervisorList() async {
-    final url = await http.get(Uri.parse('http://localhost:5000/getSupervisorList'));
+    final url =
+    await http.get(Uri.parse('http://localhost:5000/getSupervisorList'));
 
     if (url.statusCode == 200) {
       final data = jsonDecode(url.body);
@@ -68,15 +58,16 @@ class _ListaSupervisoresPageState extends State<ListaSupervisoresPage> {
       if (data['success']) {
         setState(() {
           supervisores = (data['list'] as List).map((supervisor) => Supervisor(
+            id: supervisor['id'],
             name: supervisor['name'],
-            surname: supervisor['surname']
+            surname: supervisor['surname'],
           )).toList();
         });
       } else {
-        print('Erro ao buscar a lista de supervisores: ${data['error']}');
+        print('Erro ao procurar a lista de supervisores: ${data['error']}');
       }
     } else {
-      print('Erro de conexão ao buscar a lista de supervisores');
+      print('Erro ao estabelecer conexão à lista de supervisores');
     }
   }
 
@@ -89,11 +80,35 @@ class _ListaSupervisoresPageState extends State<ListaSupervisoresPage> {
   }
 
   void _editSupervisor(Supervisor supervisor) {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (context) => EditSupervisorPage(supervisor: supervisor),
-      ),
-    );
+    if (getRole() == 'admin') {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => EditSupervisorPage(supervisor: supervisor),
+        ),
+      );
+    }
+  }
+
+  Future<void> _deleteSupervisor(Supervisor supervisor) async {
+    if (getRole() == 'admin') {
+      final int supervisorID = supervisor.id;
+      final response = await http.post(
+        Uri.parse('http://localhost:5000/deleteSupervisor'),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode({'id': supervisorID}),
+      );
+
+      if (response.statusCode == 200) {
+        // sendo excluido com sucesso da api, agr removo-o localmente da lista
+        setState(() {
+          supervisores.removeWhere((element) => element.id == supervisorID);
+        });
+      } else {
+        print('Erro ao excluir o supervisor: ${response.reasonPhrase}');
+      }
+    }
   }
 
   static List<String> getMenuItems() {
@@ -122,6 +137,7 @@ class _ListaSupervisoresPageState extends State<ListaSupervisoresPage> {
     }
   }
 
+  //Construção da lista de supervisores na interface
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -163,7 +179,8 @@ class _ListaSupervisoresPageState extends State<ListaSupervisoresPage> {
               children: [
                 ElevatedButton(
                   onPressed: () {
-                    Navigator.of(context).pushNamed('/registar_supervisores');
+                    Navigator.of(context)
+                        .pushNamed('/registar_supervisores');
                   },
                   style: ElevatedButton.styleFrom(
                     foregroundColor: Colors.white,
@@ -183,12 +200,37 @@ class _ListaSupervisoresPageState extends State<ListaSupervisoresPage> {
                   title: Text(
                     '${supervisor.name} ${supervisor.surname}',
                   ),
-                  trailing: IconButton(
-                    icon: const Icon(Icons.edit),
-                    onPressed: () {
-                      _editSupervisor(supervisor);
-                    },
-                  ),
+                  trailing: getRole() == 'admin'
+                      ? Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.edit),
+                        onPressed: () {
+                          _editSupervisor(supervisor);
+                        },
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {
+                          _deleteSupervisor(supervisor);
+                        },
+                      ),
+                    ],
+                  )
+                      : null,
+                  onTap: () {
+                    if (getRole() == 'admin') {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return DetalhesSupervisorDialog(
+                            supervisor: supervisor,
+                          );
+                        },
+                      );
+                    }
+                  },
                 );
               },
             ),
@@ -199,10 +241,50 @@ class _ListaSupervisoresPageState extends State<ListaSupervisoresPage> {
   }
 }
 
+class DetalhesSupervisorDialog extends StatelessWidget {
+  final Supervisor supervisor;
+
+  const DetalhesSupervisorDialog({Key? key, required this.supervisor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Detalhes do Supervisor'),
+      content: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Nome: ${supervisor.name}',
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Sobrenome: ${supervisor.surname}',
+              style: const TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text('Fechar'),
+        ),
+      ],
+    );
+  }
+}
+
 class EditSupervisorPage extends StatefulWidget {
   final Supervisor supervisor;
 
-  EditSupervisorPage({required this.supervisor});
+  const EditSupervisorPage({super.key, required this.supervisor});
 
   @override
   _EditSupervisorPageState createState() => _EditSupervisorPageState();
@@ -226,7 +308,7 @@ class _EditSupervisorPageState extends State<EditSupervisorPage> {
     super.dispose();
   }
 
-  //dar update no backend da atualização de qq elemento da lista
+  //Função para atualizar os dados de um supervisor
   Future<void> _updateSupervisor() async {
     final updatedName = nameController.text;
     final updatedSurname = surnameController.text;
@@ -245,15 +327,14 @@ class _EditSupervisorPageState extends State<EditSupervisorPage> {
     );
 
     if (response.statusCode == 200) {
+      // Atualização bem-sucedida
     } else {
       print('Erro na atualização do supervisor: ${response.reasonPhrase}');
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: const Color.fromRGBO(191, 191, 191, 0.8),
       appBar: PreferredSize(
@@ -276,20 +357,14 @@ class _EditSupervisorPageState extends State<EditSupervisorPage> {
               ),
             ),
             const SizedBox(height: 10),
-            CustomTextField(
+            customTextField.CustomTextField(
               labelText: 'Name',
               controller: nameController,
-              onChanged: (value) {
-                widget.supervisor.update(name: value);
-              },
             ),
             const SizedBox(height: 10),
-            CustomTextField(
+            customTextField.CustomTextField(
               labelText: 'Surname',
               controller: surnameController,
-              onChanged: (value) {
-                widget.supervisor.update(surname: value);
-              },
             ),
             const SizedBox(height: 10),
             ElevatedButton(
